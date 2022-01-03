018182af92950e44c9cb20a69715ccce51fa4fd5
Garrett Girod <girodg@hotmail.com>
2022-01-03 12:10:05 -0500

    out and optional params

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -14,10 +14,41 @@ namespace Day01
             PrintMessage(msg);
 
             int number = 5;
-            Factor(ref number, 2);
+            Factor(ref number);//factor will default to 2
+            Factor(ref number, 5);
+
+            TimeStamp(ref msg);
+            PrintMessage(msg);
+
+            SteevsInfo(out int age, out bool isCool);
+            MyFavoriteNumber(out int myNumber);
+            Console.WriteLine($"Your favorite number is {myNumber}?? WEird.");
         }
 
-        private static int Factor(ref int steev, int factor)
+        private static void MyFavoriteNumber(out int number)
+        {
+            Console.Write("What is your favorite number? ");
+           
+            if (int.TryParse(Console.ReadLine(), out number) != true)
+            {
+                Console.WriteLine("Not a number, Steve!");
+            }
+        }
+
+        static void SteevsInfo(out int age, out bool isCool)
+        {
+            age = 12;
+            isCool = true;
+        }
+
+        static void TimeStamp(ref string message)
+        {
+            //prefix the time to the message
+            //$ - interpolated string
+            message = $"{DateTime.Now}:{message}";
+        }
+
+        private static int Factor(ref int steev, int factor = 2)
         {
             steev *= factor;
             return 5;
@@ -29,13 +60,9 @@ namespace Day01
             return Console.ReadLine();
         }
 
-        static void PrintMessage(string msg)
+        static void PrintMessage(string msg = "Hello Gotham.")
         {
             Console.WriteLine(msg);
-        }
-        static void PrintMessage()
-        {
-            Console.WriteLine("Hello Gotham.");
         }
     }
 }
