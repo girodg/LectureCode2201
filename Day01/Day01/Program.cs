b51fa1c8e833bea8a61da3981a281e182037fe2b
Garrett Girod <girodg@hotmail.com>
2022-01-03 10:45:12 -0500

    added PrintMessage and GetMessage

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -6,6 +6,20 @@ namespace Day01
     {
         static void Main(string[] args)
         {
+            PrintMessage();
+
+            string msg = GetMessage();
+        }
+
+        private static string GetMessage()
+        {
+            Console.Write("Please enter a message: ");
+            return Console.ReadLine();
+        }
+
+        static void PrintMessage()
+        {
+            Console.WriteLine("Hello Gotham.");
         }
     }
 }
