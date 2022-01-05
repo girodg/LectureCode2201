7424dc30c75f9a1bbbb3fadd068bab545fd03dde
Garrett Girod <girodg@hotmail.com>
2022-01-05 10:15:55 -0500

    List Challenges

--- Day02/Day02/Program.cs
+++ Day02/Day02/Program.cs
@@ -11,6 +11,34 @@ namespace Day02
             int sum = Add(2, 5);
 
             ArrayChallenge();
+
+
+            List<int> numList = new List<int>() { 5, 13 };
+            numList.Add(42);
+            numList.Add(20);
+            for (int i = 0; i < numList.Count; i++)
+            {
+                Console.WriteLine(numList[i]);
+            }
+
+            ListChallenge();
+        }
+
+        static void ListChallenge()
+        {
+            Random rando = new Random();
+            List<double> grades = new List<double>(10);
+            PrintInfo(grades);
+            for (int i = 0; i < 10; i++)
+            {
+                grades.Add(rando.NextDouble() * 100);
+                PrintInfo(grades);
+            }
+        }
+
+        private static void PrintInfo(List<double> grades)
+        {
+            Console.WriteLine($"Count: {grades.Count}\tCapacity: {grades.Capacity}");
         }
 
         static void ArrayChallenge()
@@ -22,9 +50,6 @@ namespace Day02
             foreach (int number in numbers)
                 Console.WriteLine(number);
 
-            List<int> numList = new List<int>() { 5, 13 };
-            numList.Add(42);
-            numList.Add(20);
         }
 
         static int Add(int num1, int num2)
