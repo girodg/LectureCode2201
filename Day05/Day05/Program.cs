c5b85636028f2065f5729bf9fe3703c583c3b5f5
Garrett Girod <girodg@hotmail.com>
2022-01-12 09:43:48 -0500

    recursion

---
+++ Day05/Day05/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            Recursive(k);
+
+            for (int j = 0; j < 10; j++)
+            {
+
+            }
+            for(; ;)
+            {
+                if(true)
+                    break;
+            }
+            int i = 0;
+            while (i < 10)
+            {
+                i++;
+            }
+        }
+        static void Recursive(int num)
+        {
+            Console.WriteLine(num);
+            if (num < 10) //exit condition
+            {
+                int number = 5;
+                Recursive(num + 1);
+                Console.WriteLine(num);
+            }
+        }//returns from the method
+    }
+}
