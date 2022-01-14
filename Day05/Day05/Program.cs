69db53b2113c19c957c01f3e508309a03e1c8234
Garrett Girod <girodg@hotmail.com>
2022-01-14 09:39:46 -0500

    Fib challenge Split challenge

--- Day05/Day05/Program.cs
+++ Day05/Day05/Program.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Day05
 {
     class Program
     {
+        static Dictionary<int, long> _fib = new Dictionary<int, long>();
         static void Main(string[] args)
         {
+            _fib.Add(0, 0);
+            _fib[1] = 1;
+
+            List<int> Ns = new List<int>() { 1, 2, 4, 7, 123, 42 };
+            Split(Ns);
+
+            Stopwatch sw = new Stopwatch();
+            for (int l = 0; l < 145; l++)
+            {
+                sw.Restart();
+                long fibResult = Fib2(l);
+                sw.Stop();
+                long ms = sw.ElapsedMilliseconds;
+                Console.Write($"Fib2({l}) = {fibResult}");
+                Console.CursorLeft = 25;
+                Console.WriteLine($"time: {ms} ms");
+            }
+            Console.ReadKey();
             int k = 0;
             Recursive(k);
             Console.Clear();
@@ -123,11 +144,54 @@ namespace Day05
             return result;
         }
 
+        static long Fib(int N)
+        {
+            if (N == 0) return 0;
+            if (N == 1) return 1;
+            long result = Fib(N - 1) + Fib(N - 2);
+            return result;
+        }
+        static long Fib2(int N)
+        {
+            long result = 0;
+            if(!_fib.TryGetValue(N, out result))
+            {
+                result = Fib2(N - 1) + Fib2(N - 2);
+                _fib[N] = result;
+            }
+            return result;
+        }
+
         static void Swap(int[] nums, int index1, int index2)
         {
             int temp = nums[index1];
             nums[index1] = nums[index2];
             nums[index2] = temp;
+        }
+
+        static void Split(List<int> nums)
+        {
+            List<int> L = new List<int>();
+            List<int> R = new List<int>();
+            int mid = nums.Count / 2;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (i < mid)
+                    L.Add(nums[i]);
+                else
+                    R.Add(nums[i]);
+            }
+            Console.WriteLine("------LEFT------");
+            foreach (var item in L)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("------RIGHT------");
+            foreach (var item in R)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
