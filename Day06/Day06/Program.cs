9ffa5feaa054bbcd6cae6f21e4cb80d25569b4e2
Garrett Girod <girodg@hotmail.com>
2022-01-14 10:01:14 -0500

    LinearSearch

--- Day06/Day06/Program.cs
+++ Day06/Day06/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day06
 {
@@ -6,7 +7,32 @@ namespace Day06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> myNums = new List<int>() { 1, 6, 10, 14, 42 };
+            int searchItem = 10;
+            int foundIndex = LinearSearch(myNums, searchItem);
+            Console.WriteLine($"Found {searchItem} at index {foundIndex}");
+
+            searchItem = 13;
+            foundIndex = LinearSearch(myNums, searchItem);
+            if (foundIndex >= 0)
+                Console.WriteLine($"Found {searchItem} at index {foundIndex}");
+            else
+                Console.WriteLine($"{searchItem} was not found.");
+        }
+
+
+        static int LinearSearch(List<int> numbers, int searchItem)
+        {
+            int index = -1;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == searchItem)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
         }
     }
 }
