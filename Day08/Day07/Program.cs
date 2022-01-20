2ba6379c1fbd6f71814728b731dd4b6efe76f9c7
Garrett Girod <girodg@hotmail.com>
2022-01-20 17:04:11 -0500

    Day 08

---
+++ Day08/Day07/Program.cs
@@ -0,0 +1,54 @@
+﻿using Day07CL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car batmobile = new Car(8F, 1000);
+            //batmobile.MilesPerGallon = 8F; //calling the set passing 8 as the value
+            Console.WriteLine(batmobile.MilesPerGallon); //calling the get
+
+            int number = 5;
+            Console.WriteLine(number);
+
+            Car herbie = new Car(12F, 50);//a constructor
+            //herbie.MilesPerGallon = 12F;
+
+            Car.ModelDetails();
+            batmobile.CarDetails();
+            herbie.CarDetails();
+
+            List<string> items = new List<string>()
+            {
+                "sword",
+                "invisibility potion",
+                "shield"
+            };
+            Inventory backpack = new Inventory(15, items);
+
+            FantasyWeapon sting = new FantasyWeapon(WeaponRarity.Legendary, 200, 50, 100000);
+            int damage = sting.DoDamage();
+            Console.WriteLine($"Damage: {damage}");
+        }
+
+        void DoSomething(WinLoss winLoss)
+        {
+            switch (winLoss)
+            {
+                case WinLoss.Won:
+                    break;
+                case WinLoss.Lost:
+                    break;
+                case WinLoss.Tied:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Day08/Day07CL/FantasyWeapon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07CL
+{
+    public class FantasyWeapon
+    {
+        public WeaponRarity Rarity { get; set; }
+        public int Level { get; set; }
+        public int MaxDamage { get; set; }
+        public int Cost { get; set; }
+
+        public int DoDamage()
+        {
+            Random rando = new Random();
+            return (int)(MaxDamage * rando.NextDouble());
+        }
+
+        public FantasyWeapon(WeaponRarity rarity, int maxDamage, int level, int cost)
+        {
+            Rarity = rarity;
+            MaxDamage = maxDamage;
+            Level = level;
+            Cost = cost;
+        }
+    }
+}
