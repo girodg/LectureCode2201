bbe091d20faa0bf7d23d3507aeefe13081ab5828
Garrett Girod <girodg@hotmail.com>
2022-01-21 10:03:30 -0500

    BowWeapon

---
+++ Day08/Day07CL/BowWeapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07CL
+{
+    public class BowWeapon : FantasyWeapon
+    {
+        public int ArrowCapacity { get; set; }
+        public int ArrowCount { get; set; }
+
+        public BowWeapon(int arrowCap, int arrowCount, WeaponRarity rarity, int maxDamage, int level, int cost) 
+            : base(rarity, maxDamage, level, cost)
+        {
+            ArrowCapacity = arrowCap;
+            ArrowCount = arrowCount;
+        }
+    }
+}
